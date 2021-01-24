647090063fa1247f77d9dddff75b1a89777d2964
Denis Tumakov <dmarshaltu@gmail.com>
2021-01-24 12:22:34 +0200

    QuizBrain final update

--- Intermediate day 15-40/Quiz Game/main.py
+++ Intermediate day 15-40/Quiz Game/main.py
@@ -1,9 +1,15 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 
 question_bank = []
 for item in question_data:
-    question_bank.append(Question(q_text=item["text"], q_answer=item["answer"]))
+    question_bank.append(Question(q_text=item["question"], q_answer=item["correct_answer"]))
 
-print(question_bank[0].text)
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"You've completed the quiz!\nYore total score is: {quiz.score}/{quiz.question_number}")
