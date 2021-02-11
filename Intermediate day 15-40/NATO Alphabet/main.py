2e6fe58bdb72868f67c7cee115b52a67ec0d4d48
Denis Tumakov <dmarshaltu@gmail.com>
2021-02-11 08:10:47 +0200

    day 30 first update

--- Intermediate day 15-40/NATO Alphabet/main.py
+++ Intermediate day 15-40/NATO Alphabet/main.py
@@ -1,16 +1,24 @@
 import pandas
 
-
-# TODO 1. Create a dictionary in this format:
 nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_alphabet = {
     row.letter: row.code for (index, row) in nato_alphabet.iterrows()
 }
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_word = input("Enter a word: ").upper()
-answer = [
-    phonetic_alphabet[letter] for letter in user_word
-]
-print(answer)
+
+def generate_phonetic():
+    user_word = input("Enter a word: ").upper()
+
+    try:
+        answer = [
+            phonetic_alphabet[letter] for letter in user_word
+        ]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(answer)
+
+
+generate_phonetic()
