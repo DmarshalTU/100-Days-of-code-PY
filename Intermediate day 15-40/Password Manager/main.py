16e7d26538e7b63f6327381dc1ffbb09d0e32452
Denis Tumakov <dmarshaltu@gmail.com>
2021-02-11 09:20:59 +0200

    password manager json update

--- Intermediate day 15-40/Password Manager/main.py
+++ Intermediate day 15-40/Password Manager/main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 from random import choice, shuffle, randint
 import string
 import pyperclip
+import json
 
 
 # Functions
@@ -21,6 +22,12 @@ def generate_password():
 
 
 def add():
+    credentials = {
+        website_entry.get(): {
+            "Username": username_entry.get(),
+            "Password": password_entry.get()
+        }
+    }
     if len(website_entry.get()) == 0 or len(username_entry.get()) == 0 or len(password_entry.get()) == 0:
         messagebox.showinfo(title="Oops..", message="Make sure to fill in all the information!")
     else:
@@ -29,13 +36,36 @@ def add():
                                                                          f"User: {username_entry.get()}\n"
                                                                          f"Password: {password_entry.get()}")
         if is_ok:
-            with open("data.txt", 'a') as data:
-                data.write(f"{website_entry.get()} | "
-                           f"Username: {username_entry.get()} |"
-                           f" Password: {password_entry.get()}\n")
-            website_entry.delete(0, END)
-            username_entry.delete(0, END)
-            password_entry.delete(0, END)
+            try:
+                with open("data.json", "r") as data_file:
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(credentials, data_file, indent=4)
+            else:
+                data.update(credentials)
+                with open("data.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)
+            finally:
+                website_entry.delete(0, END)
+                username_entry.delete(0, END)
+                password_entry.delete(0, END)
+
+
+def search():
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError as error:
+        messagebox.showinfo(title="Error", message=f"{error}")
+    else:
+        if website_entry.get() in data:
+            messagebox.showinfo(title=f"{website_entry.get()}",
+                                message=f"Username: {data[website_entry.get()]['Username']}\n"
+                                        f"Password: {data[website_entry.get()]['Password']}")
+        else:
+            messagebox.showinfo(title=f"{website_entry.get()}",
+                                message=f"No such entry for {website_entry.get()}")
 
 
 # UI
@@ -61,9 +91,9 @@ password_label.grid(column=0, row=3)
 
 # Entries
 
-website_entry = Entry(width=38)
+website_entry = Entry()
 website_entry.focus()
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1)
 
 username_entry = Entry(width=38)
 username_entry.grid(column=1, row=2, columnspan=2)
@@ -78,5 +108,8 @@ generate_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=36, command=add)
 add_button.grid(column=1, row=4, columnspan=2)
+
+search_button = Button(text="Search", command=search)
+search_button.grid(column=2, row=1)
 
 window.mainloop()
